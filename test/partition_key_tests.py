45ba6d328b8f33a00b574182b72254141a57fcbe
Srinath Narayanan <srnara@microsoft.com>
2019-03-29 15:21:46 -0700

    minor change reverted

--- test/partition_key_tests.py
+++ test/partition_key_tests.py
@@ -184,5 +184,4 @@ class PartitionKeyTests(unittest.TestCase):
         read_item = self.created_collection.get_item(item=document_definition['id'], partition_key=partition_key.NonePartitionKeyValue)
         self.assertEquals(read_item['id'], document_definition['id'])
         self.created_collection.delete_item(item=document_definition['id'], partition_key=partition_key.NonePartitionKeyValue)
-        
 
